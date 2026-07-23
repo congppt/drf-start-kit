1f92965e5601859f77095a2daacc2ba65227a87e
CongPPT <congppt2002@gmail.com>
2026-07-23 15:40:20 +0700

    feat: get options return default option in 1st page if ordering params not provided

--- core/mixins/common/choice.py
+++ core/mixins/common/choice.py
@@ -6,3 +6,23 @@ class ChoiceListModelMixin:
         if self.request.query_params.get("for") == "options":
             return getattr(self, "choice_serializer_class", ChoiceSerializer)
         return super().get_serializer_class()
+
+    def filter_queryset(self, queryset):
+        if self.request.query_params.get("for") != "options" or not self._model_has_is_default(queryset.model):
+            return super().filter_queryset(queryset)
+
+        original_ordering = getattr(self, "ordering", [])
+        if isinstance(original_ordering, str):
+            ordering = [original_ordering]
+        else:
+            ordering = list(original_ordering)
+
+        self.ordering = ["-is_default", *ordering]
+        try:
+            return super().filter_queryset(queryset)
+        finally:
+            self.ordering = original_ordering
+
+    @staticmethod
+    def _model_has_is_default(model) -> bool:
+        return any(field.name == "is_default" for field in model._meta.get_fields())
